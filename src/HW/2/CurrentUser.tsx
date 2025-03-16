0f2242af8f2ac0e1c17a6016cccf9f87d6138f08
sli39 <sliva391985@gmail.com>
2025-03-16 17:48:47 +0300

    "Updating the code"

--- src/HW/2/CurrentUser.tsx
+++ src/HW/2/CurrentUser.tsx
@@ -1,12 +1,12 @@
 import { UserType } from './HW2';
 
 type CurrentUserPropsType = {
-  user: any // пропиши типизацию
+  user: UserType  // пропиши типизацию
 };
 
 export const CurrentUser = (props: CurrentUserPropsType) => {
   return (
-    <li key={props.user.id} id={`hw02-user-${props.user.id}`}>
+    <li  id={`hw02-user-${props.user.id}`}>
       <strong>{props.user.name}</strong> (Age: {props.user.age})<strong> Address: </strong>
       {props.user.address.street}, {props.user.address.city}
     </li>
